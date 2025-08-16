7a06abdfc5e50ed135b5620f20c1967cd2d7df2e
techkaka <kakatechai@gmail.com>
2025-08-16 13:52:53 +0530

    remove cart

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useNavigation } from '../Router';
 import { Button } from '../ui/button';
-import { loadSampleCartItems, checkCartStatus } from '../../utils/quickCartLoader';
-import { ShoppingBag } from 'lucide-react';
 import { HeroSection } from '../HeroSection';
 import { CategoriesSection } from '../CategoriesSection';
 import { FeaturedProducts } from '../FeaturedProducts';
@@ -15,21 +13,8 @@ export const HomePage: React.FC = () => {
     navigation.goToProducts();
   };
 
-  const handleLoadCart = () => {
-    loadSampleCartItems();
-    checkCartStatus();
-  };
-
   return (
     <div className="w-full">
-      {/* Cart Loading Button (Always Available) */}
-      <button
-        onClick={handleLoadCart}
-        className="fixed bottom-4 left-4 z-40 bg-primary hover:bg-blue1 text-white p-3 rounded-full shadow-lg transition-colors"
-        title="Load Sample Cart Items"
-      >
-        <ShoppingBag className="h-5 w-5" />
-      </button>
 
       {/* Hero Section */}
       <section className="section-compact">
